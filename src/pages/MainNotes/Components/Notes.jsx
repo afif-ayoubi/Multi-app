b3462b4dfa4704e01af8f8ebcd5832314474bb36
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-27 22:33:18 +0200

    all Notes component

---
+++ src/pages/MainNotes/Components/Notes.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import CreateNote from "./CreateNote";
+
+const Notes = () => {
+  const [inputText, setInputText] = useState("");
+  const [notes, setNotes] = useState([]);
+  const saveHandler = () => {
+    
+  };
+  return (
+    <div className="notes">
+      <CreateNote inputText={inputText} setInputText={setInputText} saveHandler={saveHandler}/>
+    </div>
+  );
+};
+export default Notes;
