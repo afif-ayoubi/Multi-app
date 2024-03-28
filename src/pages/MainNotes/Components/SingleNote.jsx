f3941435a659a1eefe4b2661a820c4959662fe6b
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-28 02:23:13 +0200

    add a single note

---
+++ src/pages/MainNotes/Components/SingleNote.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Note = ({id, text, editHandler, deleteHandler}) => {
+  return (
+    <div className='note flex column'>
+        <div className='note-body'>{text}</div>
+        <div className='note-footer flex' style={{justifyContent : "flex-end"}}>
+        <button className='note-save' onClick={() => deleteHandler(id)}>Delete</button> &nbsp;
+        <button className='note-save' onClick={() => editHandler(id, text)}>Edit</button>
+        </div>
+    </div>
+  )
+}
+
+export default Note
